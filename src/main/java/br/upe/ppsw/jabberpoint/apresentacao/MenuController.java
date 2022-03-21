468e78feb9eada8b807342242a2d2bc4311b138d
Marx <marx.santos@upe.br>
2022-03-21 20:16:30 -0300

    Refatoração da classe AboutBox
    classe apagada e movida para MenuController

--- src/main/java/br/upe/ppsw/jabberpoint/apresentacao/MenuController.java
+++ src/main/java/br/upe/ppsw/jabberpoint/apresentacao/MenuController.java
@@ -161,7 +161,7 @@ public class MenuController extends MenuBar {
 
     menuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent actionEvent) {
-        AboutBox.show(parent);
+        showAboutBox(parent);
       }
     });
 
@@ -171,6 +171,19 @@ public class MenuController extends MenuBar {
   public MenuItem mkMenuItem(String name) {
     return new MenuItem(name, new MenuShortcut(name.charAt(0)));
   }
+
+  public void showAboutBox(Frame parent) {
+	    JOptionPane.showMessageDialog(parent,
+	        "JabberPoint é um programa de apresentação de slides básico escrito em Java(tm).\n"
+	            + "Ele é disponibilizado como uma cópia livre desde que você mantenha esta informação de splash screen intacta.\n"
+	            + "Copyright (c) 1995-now by Ian F. Darwin, ian@darwinsys.com.\n"
+	            + "Adaptada por Helaine Barreiros fpara Universidade de Pernambuco, 2021 -- now.\n"
+	            + "A cópia original do autor está disponível em http://www.darwinsys.com/",
+	        "Sobre JabberPoint", JOptionPane.INFORMATION_MESSAGE);
+	  }
+
+
+
 }
 
 
