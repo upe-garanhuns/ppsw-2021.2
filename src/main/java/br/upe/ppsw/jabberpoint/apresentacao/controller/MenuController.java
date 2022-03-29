042e369ffa2928c102494fc10111033ccb80b133
kvojps <joseferreira.santosjunior@upe.br>
2022-03-28 21:37:42 -0300

    Retirando classe AboutBox

--- src/main/java/br/upe/ppsw/jabberpoint/apresentacao/controller/MenuController.java
+++ src/main/java/br/upe/ppsw/jabberpoint/apresentacao/controller/MenuController.java
@@ -33,7 +33,6 @@ import javax.swing.JOptionPane;
 import org.springframework.util.ResourceUtils;
 
 import br.upe.ppsw.jabberpoint.apresentacao.model.Presentation;
-import br.upe.ppsw.jabberpoint.apresentacao.view.AboutBox;
 import br.upe.ppsw.jabberpoint.apresentacao.view.SlideViewerComponent;
 import br.upe.ppsw.jabberpoint.apresentacao.model.*;
 
@@ -167,7 +166,13 @@ public class MenuController extends MenuBar {
 
 		menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
-				AboutBox.show(parent);
+				JOptionPane.showMessageDialog(parent,
+						"JabberPoint é um programa de apresentação de slides básico escrito em Java(tm).\n"
+								+ "Ele é disponibilizado como uma cópia livre desde que você mantenha esta informação de splash screen intacta.\n"
+								+ "Copyright (c) 1995-now by Ian F. Darwin, ian@darwinsys.com.\n"
+								+ "Adaptada por Helaine Barreiros fpara Universidade de Pernambuco, 2021 -- now.\n"
+								+ "A cópia original do autor está disponível em http://www.darwinsys.com/",
+						"Sobre JabberPoint", JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 
