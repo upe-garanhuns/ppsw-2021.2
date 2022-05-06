57d06d9e63e0319337a13e564e8a5d71346437e1
igor leonardo <aionsong456@gmail.com>
2022-05-06 18:39:43 -0300

    Adição do fomarto json usando Gson

--- src/main/java/br/upe/ppsw/jabberpoint/control/JSONFormat.java
+++ src/main/java/br/upe/ppsw/jabberpoint/control/JSONFormat.java
@@ -1,18 +1,36 @@
 package br.upe.ppsw.jabberpoint.control;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.google.gson.Gson;
+
 import br.upe.ppsw.jabberpoint.model.Presentation;
 
 public class JSONFormat implements IFilePresentationFormat {
 
 	@Override
 	public Presentation load(String fileName) {
-		// TODO Auto-generated method stub
-		return null;
+		Path path = Paths.get(fileName);
+		
+		try (Reader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+			Gson gson = new Gson();
+			Presentation presentationjson = gson.fromJson(reader, Presentation.class);
+			return presentationjson;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	
 	}
 
 	@Override
 	public void save(Presentation presentation, String fileName) {
-		// TODO Auto-generated method stub
 		
 	}
 
